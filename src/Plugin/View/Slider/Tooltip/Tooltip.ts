6110c6730a02c94b2bc2ebedb4f6595729b3b892
Khasanov Petr <pietroismad@gmail.com>
2019-12-29 23:03:06 +0300

    create: View -> Tooltip -> (F) draw_tooltip

--- src/Plugin/View/Slider/Tooltip/Tooltip.ts
+++ src/Plugin/View/Slider/Tooltip/Tooltip.ts
@@ -1,16 +1,26 @@
-export class Tooltip {
+import { Shortcut } from "../_shortcut/Shortcut";
+
+export class Tooltip extends Shortcut {
 
     tooltip?: T_View_Thumbler_or_Tooltip;
 
-    // draw_tooltip(is_draw: boolean, is_single?: boolean, orientation?: T_Slider_Orientation) {
-    //     let css_class: string = 'SRS__tooltip SRS__tooltip_'
-    //     this.tooltip = is_draw
-    //         ? is_single
-    //             ? orientation === 'horizontal'
-    //                 ? document.createElement('div').setAttribute('class', (css_class + 'horizontal'))
-    //                 : document.createElement('div').setAttribute('class', (css_class + 'vertical'))
-    //             : orientation === 'horizontal'
-    //                 ? 
-    // }
-
+    draw_tooltip(is_draw: boolean, is_single?: boolean, orientation?: T_Slider_Orientation, current_value?: T_Slider_Value): HTMLElement | undefined {
+        if(!is_draw) {
+            return undefined;
+        } 
+        this.tooltip = is_single
+            ? orientation === 'horizontal'
+                ? this.create_element_with_class('tooltip', 'horizontal')
+                : this.create_element_with_class('tooltip', 'vertical')
+            : orientation === 'horizontal'
+                ? [ this.create_element_with_class('tooltip', 'horizontal'), this.create_element_with_class('tooltip', 'horizontal') ]
+                : [ this.create_element_with_class('tooltip', 'vertical'), this.create_element_with_class('tooltip', 'vertical') ];
+        if( Array.isArray( this.tooltip ) && Array.isArray( current_value ) )  {
+            this.tooltip.forEach( (item, index) => {
+                item.innerText = String(current_value[index]);
+            });
+        } else if( !Array.isArray( this.tooltip ) ) {
+            this.tooltip.innerText = String(current_value);
+        }
+    }
 }
